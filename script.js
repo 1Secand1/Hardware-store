38526b35446ad59016cb81a995cced5d0c3c50e3
Lisand <83894494+1Secand1@users.noreply.github.com>
2023-02-05 15:15:13 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+// Слайдер
+let dots = document.getElementsByClassName("dot"),
+  dotsArea = document.getElementById("dots-block"),
+  slides = document.getElementsByClassName("slider-item"),
+  prev = document.getElementById("prev-btn"),
+  next = document.getElementById("next-btn"),
+  slideIndex = 1;
+
+showSlides(slideIndex);
+
+function showSlides(n) {
+  if (n < 1) {
+    slideIndex = slides.length;
+  } else if (n > slides.length) {
+    slideIndex = 1;
+  }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].classList.remove("dot-active");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].classList.add("dot-active");
+}
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+prev.onclick = function () {
+  plusSlides(-1);
+};
+next.onclick = function () {
+  plusSlides(1);
+};
+dotsArea.onclick = function (e) {
+  for (let i = 0; i < dots.length + 1; i++) {
+    if (e.target.classList.contains("dot") && e.target == dots[i - 1]) {
+      currentSlide(i);
+    }
+  }
+};
+
+//tabs
+
+function tabs(tabs__contentName) {
+  let i;
+  let x = document.getElementsByClassName("tabs__content-element");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById(tabs__contentName).style.display = "flex";
+}
+
+function tabsTwo(tabs__contentName, active) {
+  let i;
+  let x = document.getElementsByClassName("prices__table");
+  let y = document.getElementsByClassName("prices__item");
+
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+    y[i].classList.remove("prices__item--active");
+  }
+  document.getElementById(tabs__contentName).style.display = "block";
+  document.getElementById(active).classList.add("prices__item--active");
+}
+
